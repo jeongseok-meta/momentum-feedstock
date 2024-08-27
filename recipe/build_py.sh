41d7116208e4114e386a2bdbaf3c711962365d27
Jeongseok Lee <jeongseok@meta.com>
2024-08-26 18:10:47 -0700

    Enable building pymomentum on linux variants

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -5,6 +5,11 @@ set -euxo pipefail
 # Set CMake options for pymomentum
 export MOMENTUM_ENABLE_SIMD=OFF
 
+# Workaround for fx/gltf.h:70:13: error: narrowing conversion of '-1' from 'int' to 'char' [-Wnarrowing]
+if [[ "${target_platform}" == *aarch64 || "${target_platform}" == *ppc64le ]]; then
+  CXXFLAGS="${CXXFLAGS} -Wno-narrowing"
+fi
+
 # Install the current package with verbose output
 python -m pip install . -vv
 
