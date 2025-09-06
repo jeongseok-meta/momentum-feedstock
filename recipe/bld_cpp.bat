e4914e02046fd99f7edf6b494b4808df11fa90ab
Jeongseok (JS) Lee <142548112+jeongseok-meta@users.noreply.github.com>
2025-09-06 07:45:39 -0700

    Run test after install

--- recipe/bld_cpp.bat
+++ recipe/bld_cpp.bat
@@ -17,8 +17,8 @@ if errorlevel 1 exit 1
 cmake --build build --parallel --config Release
 if errorlevel 1 exit 1
 
-ctest --test-dir build --output-on-failure --build-config Release
+cmake --build build --parallel --config Release --target install
 if errorlevel 1 exit 1
 
-cmake --build build --parallel --config Release --target install
+ctest --test-dir build --output-on-failure --build-config Release
 if errorlevel 1 exit 1
